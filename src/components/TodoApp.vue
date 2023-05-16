e1c88dd3dc9f4c323189a3ad2d1998e2042226fe
Chuck Choi <hyunchoi94@gmail.com>
2023-05-16 14:06:17 -0700

    add dummy data and fontawesome

--- src/components/TodoApp.vue
+++ src/components/TodoApp.vue
@@ -6,5 +6,33 @@
 			<input type="text" placeholder="Enter task" class="form-control" />
 			<button class="btn btn-warning rounded-0">SUBMIT</button>
 		</div>
+
+		<!-- Task table -->
+		<table class="table table-bordered mt-5">
+			<thead>
+				<tr>
+					<th scope="col">Task</th>
+					<th scope="col">Status</th>
+					<th scope="col" class="text-center">#</th>
+					<th scope="col" class="text-center">#</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<th>Walk my dog</th>
+					<td>To-do</td>
+					<td>
+						<div class="text-center">
+							<span class="fa fa-pen"></span>
+						</div>
+					</td>
+					<td>
+						<div class="text-center">
+							<span class="fa fa-trash"></span>
+						</div>
+					</td>
+				</tr>
+			</tbody>
+		</table>
 	</div>
 </template>
